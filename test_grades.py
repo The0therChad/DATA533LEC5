f73c6440328beeb5fd90ac12240ea495f212a345
The0therChad <callmewheelz@gmail.com>
2021-12-06 10:29:08 -0800

    added class files

---
+++ test_grades.py
@@ -0,0 +1,14 @@
+import unittest
+import grades as gd
+
+
+class TestAdd(unittest.TestCase):
+    def test_gradeCalculator(self):  # test routine
+        self.assertEqual(gd.gradeCalculator(90), "A")
+        self.assertEqual(gd.gradeCalculator(80), "B")
+        self.assertEqual(gd.gradeCalculator(70), "C")
+        self.assertEqual(gd.gradeCalculator(60), "D")
+        self.assertEqual(gd.gradeCalculator(50), "F")
+
+
+unittest.main()
